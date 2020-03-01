6bf1173a17710c5041bf8d6568dfd85c3677b895
SkywalkerUA <nazarkryvonis@gmail.com>
2020-03-01 22:46:34 +0200

    Responsiveness for BarChart added

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -13,23 +13,47 @@ import {
     scaleLinear,
     scaleBand
 } from 'd3'
+import ResizeObserver from "resize-observer-polyfill";
+
+const useResizeObserver = (ref) => {
+    const [dimensions, setDimensions] = useState(null);
+    useEffect(() => {
+        const observeTarget = ref.current;
+        const resizeObserver = new ResizeObserver(entries => {
+            console.log(entries)
+            // set resized dimensions
+            entries.forEach(entry => {
+                setDimensions(entry.contentRect)
+            })
+        })
+        resizeObserver.observe(observeTarget)
+        return () => {
+            resizeObserver.unobserve(observeTarget)
+        } 
+    }, [ref])
+    return dimensions;
+}
 
 const BarChart = () => {
-
     const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75])
+
     const svgRef = useRef()
+    const wrapperRef = useRef()
+    const dimensions = useResizeObserver(wrapperRef)
 
     useEffect(() => {
         const svg = select(svgRef.current)
 
+        if (!dimensions) return; 
+
         const xScale = scaleBand()
             .domain(data.map((value, index) => index))
-            .range([0, 300])
+            .range([0, dimensions.width])
             .padding(0.5)
         
         const yScale = scaleLinear()
             .domain([0, 150])
-            .range([150, 0])
+            .range([dimensions.height, 0])
 
         const colorScale = scaleLinear()
             .domain([75, 100, 150])
@@ -42,14 +66,14 @@ const BarChart = () => {
         
         svg
             .select('.x-axis')
-            .style('transform', 'translateY(150px)')
+            .style('transform', `translateY(${dimensions.height}px)`)
             .call(xAxis)
         
         const yAxis = axisRight(yScale)
 
         svg
             .select('.y-axis')
-            .style('transform', 'translateX(300px')
+            .style('transform', `translateX(${dimensions.width}px)`)
             .call(yAxis)
 
         svg
@@ -59,22 +83,40 @@ const BarChart = () => {
             .attr('class', 'bar')
             .style('transform', 'scale(1, -1)')
             .attr('x', (value, index) => xScale(index))
-            .attr('y', -150)
+            .attr('y', -dimensions.height)
             .attr('width', xScale.bandwidth())
+            .on('mouseenter', (value, index) => {
+                svg
+                 .selectAll('.tooltip')
+                 .data([value])
+                 .join(enter => enter.append('text').attr('y', yScale(value) - 4))
+                 .attr('class', 'tooltip')
+                 .text(value)
+                 .attr('x', xScale(index) + xScale.bandwidth() / 2)
+                 .attr('text-anchor', 'middle')
+                 .transition()
+                 .attr('y', yScale(value) - 8)
+                 .attr('opacity', 1)
+            })
+            .on('mouseleave', () => svg.select('.tooltip').remove())
             .transition()
             .attr('fill', colorScale)
-            .attr('height', value => 150 - yScale(value))
+            .attr('height', value => dimensions.height - yScale(value))
             
-
-    }, [data])
+    }, [data, dimensions])
 
     return(
         <div className="bar-chart">
-            <svg ref={svgRef}>
-                <g className="x-axis" />
-                <g className="y-axis" />
-            </svg>
-            <Button onClick={() => setData(data.map(d => (d > 140) ? d = 0 : d + 10))} text="Update" />
+            <div ref={wrapperRef}>
+                <svg id="bar" ref={svgRef}>
+                    <g className="x-axis" />
+                    <g className="y-axis" />
+                </svg>
+            </div>
+            <div className="actions-btn">
+                <Button onClick={() => setData(data.map(d => (d > 140) ? d = 0 : d + 10))} text="Update" />
+                <Button onClick={() => setData([...data, Math.round(Math.random() * 100)])} text="Add Data" />
+            </div>
         </div>
     );
 };
