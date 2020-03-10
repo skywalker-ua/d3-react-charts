d6dee92908f0516f18cdacaefaf64612c17d6de6
SkywalkerUA <nazarkryvonis@gmail.com>
2020-03-10 20:46:24 +0200

    changed styling

---
+++ src/components/SelectionButtons/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './styles.css'
+import { Link } from 'react-router-dom'
+const SelectionButton = () => {
+    return(
+        <div className="selection-div">
+            <Link className="link" to="timeline-chart" >
+             <button className="select-buttons">
+                 Time Line Chart
+             </button>
+            </Link>
+            <Link className="link" to="gauge-chart" >
+             <button className="select-buttons">
+                 Gauge Chart
+             </button>
+            </Link>
+            <Link className="link" to="bar-chart" >
+             <button className="select-buttons">
+                 Bar Chart
+             </button>
+            </Link>
+            <Link className="link" to="line-chart" >
+             <button className="select-buttons">
+                 Line Chart
+             </button>
+            </Link>
+            <Link className="link" to="line-chart" >
+             <button className="select-buttons">
+                 Line Chart
+             </button>
+            </Link>
+            <Link className="link" to="line-chart" >
+             <button className="select-buttons">
+                 Line Chart
+             </button>
+            </Link>
+        </div>
+    );
+}
+
+export default SelectionButton;
